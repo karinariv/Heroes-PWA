3abb3423a2a9457418fd41d0b22e6b5869c85ff5
karinariv <karinag_riverag@hotmail.com>
2021-11-11 17:13:25 -0600

    Uploading Heroes Web App

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const heroes = {
+    render: () => {
+        const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=a1a5a31847b639232cc9d8c64b6cce63&hash=e17e110e1f14158fa71083af79b6d39c&limit=100';
+        const container = document.querySelector('#heroes-row');
+        let contentHTML = '';
+
+        fetch(urlAPI)
+            .then(res => res.json())
+            .then((json)=> {
+                for (const card of json.data.results) {
+                    contentHTML += `
+                        <div class="col-lg-2 col-md-3 col-sm-4 m-2 custom">
+                            <!-- CARDS -->
+                            <div class="card" style="width: 12rem;">
+                                <div class="image">
+                                    <img src="${card.thumbnail.path}.${card.thumbnail.extension}" class="card-img-top img-thumbnail" alt="${card.name}">
+                                </div>
+                                <div class="card-body body-card">
+                                    <p class="card-title card-name">${card.name}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                }
+                container.innerHTML = contentHTML;
+            })
+    }
+};
+heroes.render();
